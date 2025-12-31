ef4d1957fbd47f94bd136ff84062e30885a9c7d1
Christina <184446177+ckim53@users.noreply.github.com>
2025-12-31 15:38:08 -0800

    Fix cors issue

--- app.js
+++ app.js
@@ -17,8 +17,12 @@ app.use(
 	cors({
 		origin: [process.env.CLIENT_ORIGIN, process.env.ADMIN_ORIGIN],
 		credentials: true,
+		methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+		allowedHeaders: ['Content-Type', 'Authorization'],
 	}),
 );
+
+app.options('*', cors());
 
 app.use(express.json());
 
