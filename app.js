cbd6e4b3c4e0de3c9b59dc5f5b7258b0293970b3
Christina <184446177+ckim53@users.noreply.github.com>
2025-12-31 15:23:38 -0800

    Bind server to 0.0.0.0

--- app.js
+++ app.js
@@ -208,7 +208,7 @@ app.use((err, req, res, next) => {
 const PORT = process.env.PORT || 3000;
 
 if (process.env.NODE_ENV !== 'test') {
-	app.listen(PORT, () => {
+	app.listen(PORT, '0.0.0.0', () => {
 		console.log(`Server running on port ${PORT}`);
 	});
 }
